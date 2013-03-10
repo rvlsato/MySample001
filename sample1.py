204b2b7a6651e35c842448f224e7f4b7a5d3a19b
Takayoshi Sato <sato@modiphi.com>
2013-03-10 20:47:10 +0900

    test2

--- sample1.py
+++ sample1.py
@@ -4,5 +4,6 @@ import os,sys
 
 print "hello world. develop. feature. hello"
 
+# hello Sun, 10 Mar 2013 20:36:49 JST-9
 # test1
 # end
--- sample2.py
+++ sample2.py
@@ -1,5 +1,6 @@
 #!/bin/env python
 
+# hello Sun, 10 Mar 2013 20:36:59 JST-9
 # sample2
 # mod:wq
 # testtest
